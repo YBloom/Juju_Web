2a36c74c73d021de6d61e7b80126b0ef94218ef7
Carlyyb <3022402752@qq.com>
2025-07-22 11:50:40 +0800

    fix

--- plugins/Hulaquan/__init__.py
+++ plugins/Hulaquan/__init__.py
@@ -1,7 +1,9 @@
-from .main import Hulaquan
-from plugins.Hulaquan.SaojuDataManager import SaojuDataManager
-from plugins.Hulaquan.StatsDataManager import StatsDataManager
-from plugins.Hulaquan.AliasManager import AliasManager
+
 from plugins.AdminPlugin.BaseDataManager import BaseDataManager
+from plugins.Hulaquan.SaojuDataManager import SaojuDataManager
+from plugins.Hulaquan.AliasManager import AliasManager
+from plugins.Hulaquan.StatsDataManager import StatsDataManager
+from plugins.Hulaquan.HulaquanDataManager import HulaquanDataManager
+from .main import Hulaquan
 
 __all__ = ["Hulaquan"]
