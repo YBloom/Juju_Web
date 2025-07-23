2ca834fd9e9eeadc0c8d227bf6f702faa5f92842
Carlyyb <3022402752@qq.com>
2025-07-23 13:29:35 +0800

    fix

--- plugins/Hulaquan/HulaquanDataManager.py
+++ plugins/Hulaquan/HulaquanDataManager.py
@@ -607,7 +607,7 @@ class HulaquanDataManager(BaseDataManager):
     def delete_ticket(self, ticket_id, event_id=None):
         if not event_id:
             event_id = self.ticketID_to_eventID(ticket_id)
-        del self.data['events'][event_id]["ticket_details"][ticket_id]
+        return self.data['events'][event_id]["ticket_details"].pop(ticket_id, None)
     
     def ticket_details(self, event_id):
         """根据eventid获取票务数据
